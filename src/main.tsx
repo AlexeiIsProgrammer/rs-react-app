ba621178de61062efacc347c7f2928ef1631c6bd
Alexei <avenge.now.r@mail.ru>
2025-07-15 03:28:58 +0300

    init: start unit-testing task

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App.tsx';
 import ErrorBoundary from './components/ErrorBoundary/index.tsx';
 
+{
+  /* Testing */
+}
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <ErrorBoundary>
