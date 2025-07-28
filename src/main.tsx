5b6ab5f804877c7070f04dc657ec85ab2d1515e6
Alexei <avenge.now.r@mail.ru>
2025-07-28 23:22:10 +0300

    chore: download redux dependencies, add initial store

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import './index.css';
 import ErrorBoundary from './components/ErrorBoundary/index.tsx';
 import { RouterProvider } from 'react-router';
 import router from './router';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <ErrorBoundary>
-      <RouterProvider router={router} />
-    </ErrorBoundary>
+    <Provider store={store}>
+      <ErrorBoundary>
+        <RouterProvider router={router} />
+      </ErrorBoundary>
+    </Provider>
   </StrictMode>
 );
