e3405d61b37d766a4e7eedef8ac277a8763d8344
Alexei <avenge.now.r@mail.ru>
2025-08-18 06:58:03 +0300

    feat: add 404

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,9 @@ const nextConfig = {
     output: 'export',
     distDir: './dist',
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-
+    experimental: {
+        globalNotFound: true
+    }
 }
 
 export default nextConfig
